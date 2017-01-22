9e1087a3b0d0cbc04251ab4bdcee43c60d4c52b1
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-01-22 14:42:14 +0200

    Make type links on panel clickable

--- lib/panel/components/TypeLink.tsx
+++ lib/panel/components/TypeLink.tsx
@@ -1,21 +1,39 @@
 import * as React from "react";
+import { connect } from "react-redux"
 
 import { stringifyWrappers } from '../../graph';
+import { getTypeGraphSelector, TypeGraph } from '../../graph';
+import { selectElement } from '../../actions/';
 
 interface TypeLinkProps {
   name: string;
   wrappers?: string[];
+  dispatch: any;
 }
 
-export default class TypeLink extends React.Component<TypeLinkProps, void> {
+class TypeLink extends React.Component<TypeLinkProps, void> {
   render() {
     const {
       name,
       wrappers,
+      dispatch,
     } = this.props;
 
     const [leftWrap, rightWrap] = stringifyWrappers(wrappers || []);
     //TODO: add onClick
-    return <span>{leftWrap}<a className="type-name">{name}</a>{rightWrap}</span>
+    return (
+      <span>
+        {leftWrap}
+        <a
+          className="type-name"
+          onClick={() => {
+            dispatch(selectElement('TYPE::' + name));
+          }}
+        >{name}</a>
+        {rightWrap}
+      </span>
+    );
   }
 }
+
+export default connect()(TypeLink);
