e9eb7e83e73d8a75a019a4c41ea23e3b0eea7a52
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-01-25 12:47:59 +0200

    Add descriptions to TypeList

--- lib/panel/components/TypeList.tsx
+++ lib/panel/components/TypeList.tsx
@@ -2,6 +2,7 @@ import * as _ from 'lodash';
 import * as React from "react";
 
 import TypeLink from './TypeLink';
+import Markdown from './Markdown';
 
 interface TypeListProps {
   typeGraph: any;
@@ -10,12 +11,16 @@ interface TypeListProps {
 export default class TypeList extends React.Component<TypeListProps, void> {
   render() {
     const { typeGraph } = this.props;
-    const typeNames = _(typeGraph).map('name').sort().value();
+    const types = _.sortBy(typeGraph, 'name');
     return (
       <div>
-        {_.map(typeNames, name =>
-          <div key={name} className="doc-typelist-item">
-            <TypeLink name={name}/>
+        {_.map(types, type =>
+          <div key={type.id} className="doc-typelist-item">
+            <TypeLink name={type.name}/>
+            <Markdown
+              className="doc-type-description" 
+              text={type.description || 'No Description'}
+            />
           </div>
         )}
       </div>
