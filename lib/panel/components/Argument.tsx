4773736b6745e22e8b2f43392b8856ab670b6b4e
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-02-15 15:52:08 +0200

    Fix rendering of default values

--- lib/panel/components/Argument.tsx
+++ lib/panel/components/Argument.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { astFromValue, print } from 'graphql';
 
 import Markdown from './Markdown';
 import WrappedTypeName from './WrappedTypeName';
@@ -16,11 +15,11 @@ export default class Argument extends React.Component<ArgumentProps, void> {
         <span className="arg">
           <span className="arg-name">{arg.name}</span>
           <WrappedTypeName container={arg} />
-          {arg.defaultValue !== undefined &&
+          {arg.defaultValue !== null &&
             <span>
               {' = '}
               <span className="arg-default-value">
-                {print(astFromValue(arg.defaultValue, arg.type))}
+                {arg.defaultValue}
               </span>
             </span>
           }
