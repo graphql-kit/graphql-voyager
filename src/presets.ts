80551d52213e4183c56991a48d969b172ff0ddb3
Roman Hotsiy <gotsijroman@gmail.com>
2017-05-04 14:49:05 +0300

    choere: Add yelp public api to presets

--- src/presets.ts
+++ src/presets.ts
@@ -1,11 +1,11 @@
 const githubIntrospection = require('./introspection/presets/github_introspection.json');
 const swapiIntrospection = require('./introspection/presets/swapi_introspection.json');
-const brandFolderIntrospection = require('./introspection/presets/brandfolder_introspection.json');
+const yelpIntrospection = require('./introspection/presets/yelp_introspection.json');
 const hslIntrospection = require('./introspection/presets/hsl_introspection.json');
 
 window.VOYAGER_PRESETS = {
   'Star Wars': swapiIntrospection,
-  'BrandFolder': brandFolderIntrospection,
+  'Yelp': yelpIntrospection,
   'OpenTripPlanner': hslIntrospection,
   'GitHub': githubIntrospection
 }
