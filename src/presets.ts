9f0207f98fc98991ddc93bbc0e82b5b7d009b943
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-08-11 18:23:54 +0300

    Add Shopify Storefront as demo API

--- src/presets.ts
+++ src/presets.ts
@@ -1,11 +1,11 @@
 const githubIntrospection = require('./introspection/presets/github_introspection.json');
 const swapiIntrospection = require('./introspection/presets/swapi_introspection.json');
 const yelpIntrospection = require('./introspection/presets/yelp_introspection.json');
-const hslIntrospection = require('./introspection/presets/hsl_introspection.json');
+const shopifyIntrospection = require('./introspection/presets/shopify_introspection.json');
 
 window.VOYAGER_PRESETS = {
   'Star Wars': swapiIntrospection,
   'Yelp': yelpIntrospection,
-  'OpenTripPlanner': hslIntrospection,
+  'Shopify Storefront': shopifyIntrospection,
   'GitHub': githubIntrospection
 }
