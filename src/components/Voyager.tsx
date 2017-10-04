f243bf3d915b09406fa963a42c751afbc7785684
Roman Hotsiy <gotsijroman@gmail.com>
2017-10-04 10:27:12 +0300

    chore: Upgrade dependencies

--- src/components/Voyager.tsx
+++ src/components/Voyager.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PropTypes } from 'react';
+import PropTypes from 'prop-types';
 import * as _ from 'lodash';
 
 import { Provider, Store } from 'react-redux';
