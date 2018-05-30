24c895560d3e43aed8e7936c069c47e3583402ea
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-05-30 11:59:14 +0300

    Uncomment accidentally commented code

--- src/graph/viz-worker.worker.js
+++ src/graph/viz-worker.worker.js
@@ -2,11 +2,10 @@ import * as Viz from 'viz.js';
 
 onmessage = function(event) {
   try {
-    console.log(event.data.dot);
-    //console.time('Rendering Graph');
-    //var svgString = Viz(event.data.dot, {totalMemory: 2 * 16777216});
-    //console.timeEnd('Rendering Graph');
-    //postMessage({result: 'success', svgString: svgString});
+    console.time('Rendering Graph');
+    var svgString = Viz(event.data.dot, {totalMemory: 2 * 16777216});
+    console.timeEnd('Rendering Graph');
+    postMessage({result: 'success', svgString: svgString});
   }
   catch(e) {
     postMessage({result: 'failed', msg: e.toString()});
