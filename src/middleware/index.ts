eeecf7901b3c44a49921a1ee2d9d6e9116acdf73
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-06-08 01:08:25 +0300

    Add missing hapiLegacy export

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,4 +4,4 @@ import { default as hapi } from './hapi';
 import { default as koa } from './koa';
 import { default as renderVoyagerPage } from './render-voyager-page';
 
-export { express, hapi, koa, renderVoyagerPage };
+export { express, hapi, hapiLegacy, koa, renderVoyagerPage };
