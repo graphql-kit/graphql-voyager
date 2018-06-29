e06597faa7e52c316e85a3f93148cf5d26321588
Roman Hotsiy <gotsijroman@gmail.com>
2018-06-29 12:34:43 +0300

    chore: simplify webpack config

--- build/webpack.js
+++ build/webpack.js
@@ -1,33 +1,30 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals')();
 
 const root = require('./helpers').root;
 const VERSION = JSON.stringify(require('../package.json').version);
 
-const BANNER =
-`GraphQL Voyager - Represent any GraphQL API as an interactive graph
+const BANNER = `GraphQL Voyager - Represent any GraphQL API as an interactive graph
 -------------------------------------------------------------
   Version: ${VERSION}
   Repo: https://github.com/APIs-guru/graphql-voyager`;
 
-
-let baseConfig = {
-  devtool: 'cheap-source-map',
-
+module.exports = (env = {}, { mode }) => ({
   performance: {
-    hints: false
+    hints: false,
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.svg'],
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json', '.css', '.svg'],
     alias: {
-      'clipboard': 'clipboard/dist/clipboard.min.js'
-    }
+      clipboard: 'clipboard/dist/clipboard.min.js',
+    },
   },
 
-  externals: {
+  externals: env.lib ? nodeExternals : {
     react: {
       root: 'React',
       commonjs2: 'react',
@@ -41,178 +38,92 @@ let baseConfig = {
       amd: 'react-dom'
     }
   },
-  entry: {
-    'voyager': ['./src/vendor.ts', './src/index.tsx']
-  },
+  entry: ['./src/vendor.ts', './src/index.tsx'],
   output: {
     path: root('dist'),
-    filename: '[name].js',
+    filename: env.lib ? 'voyager.lib.js' : 'voyager.min.js',
     sourceMapFilename: '[file].map',
     library: 'GraphQLVoyager',
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   module: {
     rules: [
-    {
-      test: /\.tsx?$/,
-      use: [
-        'awesome-typescript-loader'
-      ],
-      exclude: [/\.(spec|e2e)\.ts$/]
-    },
-    {
-      test: /\.render\.js$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'voyager.worker.js',
-          },
-        },
-      ],
-    },
-    {
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+        exclude: [/\.(spec|e2e)\.ts$/],
+      },
+      {
+        test: /\.render\.js$/,
         use: [
           {
-            loader: 'css-loader',
+            loader: 'file-loader',
             options: {
-              sourceMap: true,
-              minimize: true
+              name: 'voyager.worker.js',
             },
           },
-          'postcss-loader?sourceMap'
-        ]
-      }),
-      exclude: /variables\.css$/, 
-    },
-    {
-      test: /variables\.css$/,
-      loader: 'postcss-variables-loader?es5=1'
-    },  
-    {
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              'transform-es2015-classes',
-              'transform-es2015-block-scoping',
-              'transform-es2015-arrow-functions',
-              'transform-es2015-destructuring'
-            ]
-          }
-        },
-        {
-          loader: 'react-svg-loader',
-          options: {
-            jsx: false,
-            svgo: {
-              plugins: [{mergePaths: false}]
-            }
-          }
-        }
-      ]
-    }]
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                minimize: true,
+              },
+            },
+            'postcss-loader?sourceMap',
+          ],
+        }),
+        exclude: /variables\.css$/,
+      },
+      {
+        test: /variables\.css$/,
+        loader: 'postcss-variables-loader?es5=1',
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                'transform-es2015-classes',
+                'transform-es2015-block-scoping',
+                'transform-es2015-arrow-functions',
+                'transform-es2015-destructuring',
+              ],
+            },
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: false,
+              svgo: {
+                plugins: [{ mergePaths: false }],
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'VERSION': VERSION
+      VERSION: VERSION,
     }),
 
     new ExtractTextPlugin({
       filename: 'voyager.css',
-      allChunks: true
+      allChunks: true,
     }),
-    new webpack.BannerPlugin(BANNER)
+
+    new webpack.BannerPlugin(BANNER),
   ],
-  node: {
-    console: false,
-    global: false,
-    process: 'mock',
-    Buffer: false,
-    fs: true,
-    global: true,
-
-    crypto: 'empty',
-    fs: 'empty',
-    path: true,
-    clearImmediate: false,
-    setImmediate: false
-  }
-}
-
-let minConfig = Object.assign({}, baseConfig);
-minConfig.output = {
-  path: root('dist'),
-  filename: '[name].min.js',
-  sourceMapFilename: '[file].map',
-  library: 'GraphQLVoyager',
-  libraryTarget: 'umd',
-  umdNamedDefine: true
-};
-
-minConfig.plugins = minConfig.plugins.slice();
-minConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  compress: {
-    warnings: false,
-    screw_ie8: true
-  },
-  sourceMap: true
-}));
-
-let libConfig = Object.assign({}, baseConfig);
-libConfig.externals = {
-  react: {
-    root: 'React',
-    commonjs2: 'react',
-    commonjs: 'react',
-    amd: 'react'
-  },
-  'react-dom': {
-    root: 'ReactDOM',
-    commonjs2: 'react-dom',
-    commonjs: 'react-dom',
-    amd: 'react-dom'
-  },
-  lodash : {
-    commonjs: "lodash",
-    commonjs2: 'lodash',
-    amd: "lodash",
-    root: "_"
-  },
-  graphql: 'graphql',
-  "@f/animate": "@f/animate",
-  "classnames": "classnames",
-  "clipboard": "clipboard",
-  "marked": "marked",
-  "react-modal": "react-modal",
-  "react-redux": "react-redux",
-  "redux": "redux",
-  "redux-thunk": "redux-thunk",
-  "reselect": "reselect",
-  "svg-pan-zoom": "svg-pan-zoom"
-}
-
-libConfig.output = {
-  path: root('dist'),
-  filename: '[name].lib.js',
-  sourceMapFilename: '[file].map',
-  library: 'GraphQLVoyager',
-  libraryTarget: 'umd',
-  umdNamedDefine: true
-};
-
-
-module.exports = [baseConfig, minConfig, libConfig];
+});
