9877d7556535209e1538374de6d21f58388f6fea
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-09-06 18:49:39 +0300

    Move preprocessVizSvg closer to Worker code

--- src/graph/svg-renderer.ts
+++ src/graph/svg-renderer.ts
@@ -1,12 +1,22 @@
+import * as _ from 'lodash';
 import { getDotSelector } from './dot';
 import { observeStore } from '../redux';
 import { svgRenderingFinished, reportError } from '../actions';
 
-import { loadWorker as defaultLoadWorker } from '../utils/';
+import {
+  forEachNode,
+  loadWorker as defaultLoadWorker,
+  stringToSvg,
+} from '../utils/';
+
 import { WorkerCallback } from '../utils/types';
 
 import Viz from 'viz.js';
 import defaultWorkerURI from 'viz.js/full.render.js';
+
+const RelayIconSvg = require('!!svg-as-symbol-loader?id=RelayIcon!../components/icons/relay-icon.svg');
+const svgns = 'http://www.w3.org/2000/svg';
+const xlinkns = 'http://www.w3.org/1999/xlink';
 
 export class SVGRender {
   unsubscribe: any;
@@ -34,8 +44,10 @@ export class SVGRender {
     console.time('Rendering Graph');
     this.viz
       .renderString(dot)
-      .then(svg => {
+      .then(rawSVG => {
+        const svg = preprocessVizSVG(rawSVG);
         this.store.dispatch(svgRenderingFinished(svg));
+
         console.timeEnd('Rendering Graph');
       })
       .catch(error => {
@@ -43,4 +55,95 @@ export class SVGRender {
         this.store.dispatch(reportError(msg));
       });
   }
+}
+
+function preprocessVizSVG(svgString: string) {
+  //Add Relay Icon
+  svgString = svgString.replace(/<svg [^>]*>/, '$&' + RelayIconSvg);
+
+  let svg = stringToSvg(svgString);
+
+  forEachNode(svg, 'a', $a => {
+    let $g = $a.parentNode;
+
+    var $docFrag = document.createDocumentFragment();
+    while ($a.firstChild) {
+      let $child = $a.firstChild;
+      $docFrag.appendChild($child);
+    }
+
+    $g.replaceChild($docFrag, $a);
+
+    $g.id = $g.id.replace(/^a_/, '');
+  });
+
+  forEachNode(svg, 'title', $el => $el.remove());
+
+  var edgesSources = {};
+  forEachNode(svg, '.edge', $edge => {
+    let [from, to] = $edge.id.split(' => ');
+    $edge.removeAttribute('id');
+    $edge.setAttribute('data-from', from);
+    $edge.setAttribute('data-to', to);
+    edgesSources[from] = true;
+  });
+
+  forEachNode(svg, '[id]', $el => {
+    let [tag, ...restOfId] = $el.id.split('::');
+    if (_.size(restOfId) < 1) return;
+
+    $el.classList.add(tag.toLowerCase().replace(/_/, '-'));
+  });
+
+  forEachNode(svg, 'g.edge path', $path => {
+    let $newPath = $path.cloneNode() as HTMLElement;
+    $newPath.classList.add('hover-path');
+    $newPath.removeAttribute('stroke-dasharray');
+    $path.parentNode.appendChild($newPath);
+  });
+
+  forEachNode(svg, '.field', $field => {
+    let texts = $field.querySelectorAll('text');
+    texts[0].classList.add('field-name');
+    //Remove spaces used for text alligment
+    texts[1].remove();
+
+    if (edgesSources[$field.id]) $field.classList.add('edge-source');
+
+    for (var i = 2; i < texts.length; ++i) {
+      var str = texts[i].innerHTML;
+      if (str === '{R}') {
+        const $iconPlaceholder = texts[i];
+        const height = 22;
+        const width = 22;
+        const $useRelayIcon = document.createElementNS(svgns, 'use');
+        $useRelayIcon.setAttributeNS(xlinkns, 'href', '#RelayIcon');
+        $useRelayIcon.setAttribute('width', `${width}px`);
+        $useRelayIcon.setAttribute('height', `${height}px`);
+
+        //FIXME: remove hardcoded offset
+        const y = parseInt($iconPlaceholder.getAttribute('y')) - 15;
+        $useRelayIcon.setAttribute('x', $iconPlaceholder.getAttribute('x'));
+        $useRelayIcon.setAttribute('y', y.toString());
+        $field.replaceChild($useRelayIcon, $iconPlaceholder);
+        continue;
+      }
+
+      texts[i].classList.add('field-type');
+      if (edgesSources[$field.id] && !/[\[\]\!]/.test(str)) texts[i].classList.add('type-link');
+    }
+  });
+
+  forEachNode(svg, '.derived-type', $derivedType => {
+    $derivedType.classList.add('edge-source');
+    $derivedType.querySelector('text').classList.add('type-link');
+  });
+
+  forEachNode(svg, '.possible-type', $possibleType => {
+    $possibleType.classList.add('edge-source');
+    $possibleType.querySelector('text').classList.add('type-link');
+  });
+
+  const serializer = new XMLSerializer();
+  return serializer.serializeToString(svg);
 }
--- src/graph/viewport.ts
+++ src/graph/viewport.ts
@@ -4,14 +4,9 @@ import * as animate from '@f/animate';
 
 import * as Actions from '../actions';
 import { observeStore } from '../redux';
-
 import { removeClass, forEachNode, stringToSvg } from '../utils/';
 
 import { typeNameToId } from '../introspection';
-
-const RelayIconSvg = require('!!svg-as-symbol-loader?id=RelayIcon!../components/icons/relay-icon.svg');
-const svgns = 'http://www.w3.org/2000/svg';
-const xlinkns = 'http://www.w3.org/1999/xlink';
 
 export class Viewport {
   $svg: SVGElement;
@@ -70,7 +65,7 @@ export class Viewport {
 
   display(svgString) {
     this.clear();
-    this.$svg = preprocessVizSvg(svgString);
+    this.$svg = stringToSvg(svgString);
     this.container.appendChild(this.$svg);
     // run on the next tick
     setTimeout(() => {
@@ -248,96 +243,6 @@ export class Viewport {
       // skip
     }
   }
-}
-
-export function preprocessVizSvg(svgString: string) {
-  //Add Relay Icon
-  svgString = svgString.replace(/<svg [^>]*>/, '$&' + RelayIconSvg);
-
-  let svg = stringToSvg(svgString);
-
-  forEachNode(svg, 'a', $a => {
-    let $g = $a.parentNode;
-
-    var $docFrag = document.createDocumentFragment();
-    while ($a.firstChild) {
-      let $child = $a.firstChild;
-      $docFrag.appendChild($child);
-    }
-
-    $g.replaceChild($docFrag, $a);
-
-    $g.id = $g.id.replace(/^a_/, '');
-  });
-
-  forEachNode(svg, 'title', $el => $el.remove());
-
-  var edgesSources = {};
-  forEachNode(svg, '.edge', $edge => {
-    let [from, to] = $edge.id.split(' => ');
-    $edge.removeAttribute('id');
-    $edge.setAttribute('data-from', from);
-    $edge.setAttribute('data-to', to);
-    edgesSources[from] = true;
-  });
-
-  forEachNode(svg, '[id]', $el => {
-    let [tag, ...restOfId] = $el.id.split('::');
-    if (_.size(restOfId) < 1) return;
-
-    $el.classList.add(tag.toLowerCase().replace(/_/, '-'));
-  });
-
-  forEachNode(svg, 'g.edge path', $path => {
-    let $newPath = $path.cloneNode() as HTMLElement;
-    $newPath.classList.add('hover-path');
-    $newPath.removeAttribute('stroke-dasharray');
-    $path.parentNode.appendChild($newPath);
-  });
-
-  forEachNode(svg, '.field', $field => {
-    let texts = $field.querySelectorAll('text');
-    texts[0].classList.add('field-name');
-    //Remove spaces used for text alligment
-    texts[1].remove();
-
-    if (edgesSources[$field.id]) $field.classList.add('edge-source');
-
-    for (var i = 2; i < texts.length; ++i) {
-      var str = texts[i].innerHTML;
-      if (str === '{R}') {
-        const $iconPlaceholder = texts[i];
-        const height = 22;
-        const width = 22;
-        const $useRelayIcon = document.createElementNS(svgns, 'use');
-        $useRelayIcon.setAttributeNS(xlinkns, 'href', '#RelayIcon');
-        $useRelayIcon.setAttribute('width', `${width}px`);
-        $useRelayIcon.setAttribute('height', `${height}px`);
-
-        //FIXME: remove hardcoded offset
-        const y = parseInt($iconPlaceholder.getAttribute('y')) - 15;
-        $useRelayIcon.setAttribute('x', $iconPlaceholder.getAttribute('x'));
-        $useRelayIcon.setAttribute('y', y.toString());
-        $field.replaceChild($useRelayIcon, $iconPlaceholder);
-        continue;
-      }
-
-      texts[i].classList.add('field-type');
-      if (edgesSources[$field.id] && !/[\[\]\!]/.test(str)) texts[i].classList.add('type-link');
-    }
-  });
-
-  forEachNode(svg, '.derived-type', $derivedType => {
-    $derivedType.classList.add('edge-source');
-    $derivedType.querySelector('text').classList.add('type-link');
-  });
-
-  forEachNode(svg, '.possible-type', $possibleType => {
-    $possibleType.classList.add('edge-source');
-    $possibleType.querySelector('text').classList.add('type-link');
-  });
-
-  return svg;
 }
 
 function getParent(elem: Element, className: string): Element | null {
