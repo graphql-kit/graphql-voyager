dc480c03543819d3faea0d6183aa88d3c3b825f5
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-03-12 17:08:22 +0200

    Simplify arg passing in TypeDoc

--- src/components/doc-explorer/TypeDoc.tsx
+++ src/components/doc-explorer/TypeDoc.tsx
@@ -33,106 +33,106 @@ export default class TypeDoc extends React.Component<TypeDocProps> {
     itemComponent.scrollIntoViewIfNeeded();
   }
 
-  renderTypesDef(type: SimplifiedTypeWithIDs, typeGraph, selectedId, onSelectEdge) {
-    let typesTitle;
-    let types: {
-      id: string;
-      type: SimplifiedTypeWithIDs;
-    }[];
-
-    switch (type.kind) {
-      case 'UNION':
-        typesTitle = 'possible types';
-        types = type.possibleTypes;
-        break;
-      case 'INTERFACE':
-        typesTitle = 'implementations';
-        types = type.derivedTypes;
-        break;
-      case 'OBJECT':
-        typesTitle = 'implements';
-        types = type.interfaces;
-        break;
-      default:
-        return null;
-    }
-
-    types = _.filter(types, type => typeGraph.nodes[type.type.id] !== undefined);
-    if (_.isEmpty(types)) return null;
-
-    return (
-      <div className="doc-category">
-        <div className="title">{typesTitle}</div>
-        {_.map(types, type => {
-          let props: any = {
-            key: type.id,
-            className: classNames('item', {
-              '-selected': type.id === selectedId,
-            }),
-            onClick: () => onSelectEdge(type.id),
-          };
-          if (type.id === selectedId) props.ref = 'selectedItem';
-          return (
-            <div {...props}>
-              <TypeLink type={type.type} />
-              <Description text={type.type.description} className="-linked-type" />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-
-  renderFields(type: SimplifiedTypeWithIDs, selectedId: string, onSelectEdge) {
-    if (_.isEmpty(type.fields)) return null;
-
-    return (
-      <div className="doc-category">
-        <div className="title">{'fields'}</div>
-        {_.map(type.fields, field => {
-          let props: any = {
-            key: field.name,
-            className: classNames('item', {
-              '-selected': field.id === selectedId,
-              '-with-args': !_.isEmpty(field.args),
-            }),
-            onClick: () => onSelectEdge(field.id),
-          };
-          if (field.id === selectedId) props.ref = 'selectedItem';
-          return (
-            <div {...props}>
-              <a className="field-name">{field.name}</a>
-              <span
-                className={classNames('args-wrap', {
-                  '-empty': _.isEmpty(field.args),
-                })}
-              >
-                {!_.isEmpty(field.args) && (
-                  <span key="args" className="args">
-                    {_.map(field.args, arg => (
-                      <Argument key={arg.name} arg={arg} expanded={field.id === selectedId} />
-                    ))}
-                  </span>
-                )}
-              </span>
-              <WrappedTypeName container={field} />
-              {field.isDeprecated && <span className="doc-alert-text">{' (DEPRECATED)'}</span>}
-              <Markdown text={field.description} className="description-box -field" />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-
   render() {
     const { selectedType, selectedEdgeID, typeGraph, onSelectEdge } = this.props;
     return (
       <div className="type-doc">
         <Description className="-doc-type" text={selectedType.description} />
-        {this.renderTypesDef(selectedType, typeGraph, selectedEdgeID, onSelectEdge)}
-        {this.renderFields(selectedType, selectedEdgeID, onSelectEdge)}
+        {renderTypesDef(selectedType, selectedEdgeID)}
+        {renderFields(selectedType, selectedEdgeID)}
       </div>
     );
+
+    function renderTypesDef(type: SimplifiedTypeWithIDs, selectedId) {
+      let typesTitle;
+      let types: {
+        id: string;
+        type: SimplifiedTypeWithIDs;
+      }[];
+
+      switch (type.kind) {
+        case 'UNION':
+          typesTitle = 'possible types';
+          types = type.possibleTypes;
+          break;
+        case 'INTERFACE':
+          typesTitle = 'implementations';
+          types = type.derivedTypes;
+          break;
+        case 'OBJECT':
+          typesTitle = 'implements';
+          types = type.interfaces;
+          break;
+        default:
+          return null;
+      }
+
+      types = _.filter(types, type => typeGraph.nodes[type.type.id] !== undefined);
+      if (_.isEmpty(types)) return null;
+
+      return (
+        <div className="doc-category">
+          <div className="title">{typesTitle}</div>
+          {_.map(types, type => {
+            let props: any = {
+              key: type.id,
+              className: classNames('item', {
+                '-selected': type.id === selectedId,
+              }),
+              onClick: () => onSelectEdge(type.id),
+            };
+            if (type.id === selectedId) props.ref = 'selectedItem';
+            return (
+              <div {...props}>
+                <TypeLink type={type.type} />
+                <Description text={type.type.description} className="-linked-type" />
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    function renderFields(type: SimplifiedTypeWithIDs, selectedId: string) {
+      if (_.isEmpty(type.fields)) return null;
+
+      return (
+        <div className="doc-category">
+          <div className="title">{'fields'}</div>
+          {_.map(type.fields, field => {
+            let props: any = {
+              key: field.name,
+              className: classNames('item', {
+                '-selected': field.id === selectedId,
+                '-with-args': !_.isEmpty(field.args),
+              }),
+              onClick: () => onSelectEdge(field.id),
+            };
+            if (field.id === selectedId) props.ref = 'selectedItem';
+            return (
+              <div {...props}>
+                <a className="field-name">{field.name}</a>
+                <span
+                  className={classNames('args-wrap', {
+                    '-empty': _.isEmpty(field.args),
+                  })}
+                >
+                  {!_.isEmpty(field.args) && (
+                    <span key="args" className="args">
+                      {_.map(field.args, arg => (
+                        <Argument key={arg.name} arg={arg} expanded={field.id === selectedId} />
+                      ))}
+                    </span>
+                  )}
+                </span>
+                <WrappedTypeName container={field} />
+                {field.isDeprecated && <span className="doc-alert-text">{' (DEPRECATED)'}</span>}
+                <Markdown text={field.description} className="description-box -field" />
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
   }
 }
