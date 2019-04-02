48ebc7e2310aea681f5e5b9e5296045cd280a8a7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-04-02 17:21:22 +0300

    Inline "react-string-replace" dependency

--- src/utils/highlight.tsx
+++ src/utils/highlight.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import * as reactStringReplace from 'react-string-replace';
+import * as _ from 'lodash';
 
 export function highlightTerm(content: string, term: string) {
-  return term
-    ? reactStringReplace(content, term, (match, i) => (
-        <span key={i} style={{ backgroundColor: '#ffff03' }}>
-          {match}
-        </span>
-      ))
-    : content;
+  if (!term) {
+    return content;
+  }
+
+  var re = new RegExp('(' + _.escapeRegExp(term) + ')', 'gi');
+  var result: any = content.split(re);
+
+  // Apply highlight to all odd elements
+  for (var i = 1, length = result.length; i < length; i += 2) {
+    result[i] = <span key={i} style={{ backgroundColor: '#ffff03' }}>
+      {result[i]}
+    </span>;
+  }
+
+  return result;
 }
