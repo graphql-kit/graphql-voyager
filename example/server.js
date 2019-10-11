101aa49393676fa5dd0f2b51b34e939a87fda93d
Sukhpal Saini <bdcorps@gmail.com>
2019-10-11 16:24:50 +0000

    update example server to use ENV PORT

--- example/server.js
+++ example/server.js
@@ -3,11 +3,13 @@ const graphqlHTTP = require('express-graphql');
 
 const schema = require('./schema');
 
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3005;
+
 const app = express();
 app.use(express.static(__dirname));
 app.use('/graphql', graphqlHTTP(() => ({ schema })));
 
-app.listen(0, function() {
-  const port = this.address().port;
+
+app.listen(port, function() {
   console.log(`Started on http://localhost:${port}/`);
 });
