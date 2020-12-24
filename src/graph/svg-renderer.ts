345a1c1dbf7221ad8df735e1e342c99a0fe94590
grace <grace.c.youngblood@gmail.com>
2020-12-24 14:34:22 -0500

    [error] drop `viz.js` dependency, add `dagre-d3` and sample code.
    See:
    https://dagrejs.github.io/project/dagre-d3/latest/demo/interactive-demo.html
    https://github.com/dagrejs/dagre-d3/blob/master/demo/interactive-demo.html

--- src/graph/svg-renderer.ts
+++ src/graph/svg-renderer.ts
@@ -46,6 +46,65 @@ export class SVGRender {
   }
 }
 
+/*
+var inputGraph = document.querySelector("#inputGraph");
+
+var graphLink = d3.select("#graphLink");
+
+var oldInputGraphValue;
+
+var graphRE = /[?&]graph=([^&]+)/;
+var graphMatch = window.location.search.match(graphRE);
+if (graphMatch) {
+  inputGraph.value = decodeURIComponent(graphMatch[1]);
+}
+var debugAlignmentRE = /[?&]alignment=([^&]+)/;
+var debugAlignmentMatch = window.location.search.match(debugAlignmentRE);
+var debugAlignment;
+if (debugAlignmentMatch) debugAlignment = debugAlignmentMatch[1];
+
+// Set up zoom support
+var svg = d3.select("svg"),
+    inner = d3.select("svg g"),
+    zoom = d3.zoom().on("zoom", function() {
+      inner.attr("transform", d3.event.transform);
+    });
+svg.call(zoom);
+
+// Create and configure the renderer
+var render = dagreD3.render();
+
+  var g;
+function tryDraw() {
+  if (oldInputGraphValue !== inputGraph.value) {
+    inputGraph.setAttribute("class", "");
+    oldInputGraphValue = inputGraph.value;
+    try {
+      g = graphlibDot.read(inputGraph.value);
+    } catch (e) {
+      inputGraph.setAttribute("class", "error");
+      throw e;
+    }
+
+    // Save link to new graph
+    graphLink.attr("href", graphToURL());
+
+    // Set margins, if not present
+    if (!g.graph().hasOwnProperty("marginx") &&
+        !g.graph().hasOwnProperty("marginy")) {
+      g.graph().marginx = 20;
+      g.graph().marginy = 20;
+    }
+
+    g.graph().transition = function(selection) {
+      return selection.transition().duration(500);
+    };
+
+    // Render the graph into svg g
+    d3.select("svg g").call(render, g);
+  }
+}*/
+
 function preprocessVizSVG(svgString: string) {
   //Add Relay and Deprecated icons
   svgString = svgString.replace(/<svg [^>]*>/, '$&' + RelayIconSvg);
