12c2aa0ef0c783d1ea84c3a87e7146191a48ad91
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-07-24 12:38:06 +0300

    Simplify detection of worker path

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,3 @@
-import * as path from 'path';
-
-// similar to node __dirname
-export var __dirname;
 export * from './dom-helpers';
 export * from './highlight';
 
@@ -19,7 +15,7 @@ export function isMatch(sourceText: string, searchValue: string) {
 }
 
 export function loadWorker(path: string, relative: boolean): Promise<Worker> {
-  const url = relative ? __dirname + '/' + path : path;
+  const url = relative ? relativeToJsUrlPathname(path) : path;
   return fetch(url)
     .then((response) => response.text())
     .then((payload) => {
@@ -34,21 +30,11 @@ export function loadWorker(path: string, relative: boolean): Promise<Worker> {
     });
 }
 
-/*
-  get current script URL
-*/
-function getJsUrl(): string {
-  var id = +new Date() + Math.random();
+function relativeToJsUrlPathname(path: string): string {
   try {
-    // write empty script to the document. It will get placed directly after the current script
-    document.write(`<script id="dummy${id}"><\/script>`);
-    // find appended script and return src of the previous script which is the current script
-    return (
-      document.getElementById('dummy' + id).previousSibling as HTMLScriptElement
-    ).src;
+    const url = new URL(path, (document.currentScript as HTMLScriptElement).src);
+    return url.pathname;
   } catch (e) {
-    return '';
+    return path;
   }
 }
-
-__dirname = path.dirname(getJsUrl());
