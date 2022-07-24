6d49ae472179a5a70eb2662f2a191158d5d892bd
Stefano Cislaghi <s.cislaghi@gmail.com>
2022-07-24 16:52:53 +0100

    chore: eslint fix

--- src/utils/highlight.tsx
+++ src/utils/highlight.tsx
@@ -1,16 +1,17 @@
-/* eslint-disable */
 import * as _ from 'lodash';
+import { ReactElement } from 'react';
 
 export function highlightTerm(content: string, term: string) {
   if (!term) {
     return content;
   }
 
-  var re = new RegExp('(' + _.escapeRegExp(term) + ')', 'gi');
-  var result: any = content.split(re);
+  const re = new RegExp('(' + _.escapeRegExp(term) + ')', 'gi');
+  const result: string[] = content.split(re);
 
   // Apply highlight to all odd elements
-  for (var i = 1, length = result.length; i < length; i += 2) {
+  for (let i = 1, length = result.length; i < length; i += 2) {
+    const result: ReactElement | null = null;
     result[i] = <mark key={i}>{result[i]}</mark>;
   }
 
