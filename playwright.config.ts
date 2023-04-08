c18caddd78d9eba433d5e014e9ece7c3c9615738
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2023-04-09 01:12:28 +0300

    second attempt to fix flaky test (#278)

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,13 +7,13 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 10000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
