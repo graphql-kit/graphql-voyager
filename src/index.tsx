8238efb5d237593f6b2765679de93b394acf3a3a
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2023-04-24 14:27:36 +0300

    cleanup lib exports (#288)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as ReactDOM from 'react-dom';
 
 import { Voyager, VoyagerProps } from './components';
-import { voyagerIntrospectionQuery } from './utils/introspection-query';
 
-function init(element: HTMLElement, options: VoyagerProps) {
+export function init(element: HTMLElement, options: VoyagerProps) {
   ReactDOM.render(<Voyager {...options} />, element);
 }
 
-export { Voyager as GraphQLVoyager, init, Voyager, voyagerIntrospectionQuery };
+export { Voyager } from './components';
+// FIXME: remove in next breaking version
+export { Voyager as GraphQLVoyager };
+
+export { voyagerIntrospectionQuery } from './utils/introspection-query';
