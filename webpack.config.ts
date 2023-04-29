ab8b9da2546c3ad7123cae480a698c6bba200ff0
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2023-04-30 00:42:43 +0300

    npm: add 'voyager.standalone.js' as fully standalone bundle

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,89 +11,118 @@ const BANNER = `GraphQL Voyager - Represent any GraphQL API as an interactive gr
   Repo: ${packageJSON.repository.url}`;
 
 interface Env {
+  min?: boolean;
   lib?: boolean;
+  standalone?: boolean;
 }
 
 export default function buildWebpackConfig(env: Env): webpack.Configuration {
-  return {
-    performance: {
-      hints: false,
-    },
+  if (env.lib === true) {
+    return {
+      ...baseConfig,
+      externals: NodeExternals(),
+      output: {
+        ...baseConfig.output,
+        filename: 'voyager.lib.js',
+      },
+    };
+  }
 
-    optimization: {
-      minimize: !env.lib,
-    },
+  if (env.standalone === true) {
+    return {
+      ...baseConfig,
+      optimization: { minimize: true },
+      externals: undefined,
+      output: {
+        ...baseConfig.output,
+        filename: 'voyager.standalone.js',
+      },
+    };
+  }
 
-    resolve: {
-      extensions: ['.ts', '.tsx', '.mjs', '.js', '.json', '.css', '.svg'],
-      alias: { '../../worker': '../../worker-dist' },
-    },
-
-    externals: env.lib
-      ? NodeExternals()
-      : {
-          react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react',
-          },
-          'react-dom': {
-            root: 'ReactDOM',
-            commonjs2: 'react-dom',
-            commonjs: 'react-dom',
-            amd: 'react-dom',
-          },
+  // TODO: delete in next major version
+  if (env.min === true) {
+    return {
+      ...baseConfig,
+      optimization: { minimize: true },
+      externals: {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react',
         },
-    entry: './src/index.tsx',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: env.lib ? 'voyager.lib.js' : 'voyager.min.js',
-      sourceMapFilename: '[file].map',
-      library: 'GraphQLVoyager',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: {
-            loader: 'ts-loader',
-            options: { compilerOptions: { noEmit: false } },
-          },
-          exclude: [/\.(spec|e2e)\.ts$/],
+        'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom',
         },
-        {
-          test: /\.css$/,
-          exclude: /variables\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: { sourceMap: true },
-            },
-            { loader: 'postcss-loader', options: { sourceMap: true } },
-          ],
-        },
-        {
-          test: /variables\.css$/,
-          use: [{ loader: 'postcss-variables-loader?es5=1' }],
-        },
-        {
-          test: /\.svg$/,
-          issuer: /\.tsx?$/,
-          use: [{ loader: '@svgr/webpack' }],
-        },
-      ],
-    },
-
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'voyager.css',
-      }),
-
-      new webpack.BannerPlugin(BANNER),
-    ],
-  };
+      },
+      output: {
+        ...baseConfig.output,
+        filename: 'voyager.min.js',
+      },
+    };
+  }
+  throw new Error('Please specify correct env');
 }
+
+const baseConfig: webpack.Configuration = {
+  performance: {
+    hints: false,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json', '.css', '.svg'],
+    alias: { '../../worker': '../../worker-dist' },
+  },
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    sourceMapFilename: '[file].map',
+    library: 'GraphQLVoyager',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: { compilerOptions: { noEmit: false } },
+        },
+        exclude: [/\.(spec|e2e)\.ts$/],
+      },
+      {
+        test: /\.css$/,
+        exclude: /variables\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+        ],
+      },
+      {
+        test: /variables\.css$/,
+        use: [{ loader: 'postcss-variables-loader?es5=1' }],
+      },
+      {
+        test: /\.svg$/,
+        issuer: /\.tsx?$/,
+        use: [{ loader: '@svgr/webpack' }],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'voyager.css',
+    }),
+
+    new webpack.BannerPlugin(BANNER),
+  ],
+};
