ae4bed216b91a24fe65c2961396c142f1e480e73
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2023-05-11 15:52:12 +0300

    Remove 'init' and 'GraphQLVoyager' exports (#317)

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,2 @@
-import * as ReactDOM from 'react-dom';
-
-import { Voyager, VoyagerProps } from './components';
-
-// FIXME: remove in next breaking version
-/** @deprecated please use renderVoyagerPage **/
-export function init(element: HTMLElement, options: VoyagerProps) {
-  ReactDOM.render(<Voyager {...options} />, element);
-}
-
 export { Voyager, type VoyagerProps } from './components';
-
-// FIXME: remove in next breaking version
-export { Voyager as GraphQLVoyager };
-
 export { voyagerIntrospectionQuery } from './utils/introspection-query';
