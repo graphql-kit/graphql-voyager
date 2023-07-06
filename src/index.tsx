8f40d4c31828d01a4f7bdaccfe3684553e74880d
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-07-06 11:53:59 -0500

    Export sdlToIntrospection

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export { Voyager, type VoyagerProps } from './components';
 export { voyagerIntrospectionQuery } from './utils/introspection-query';
+export { sdlToIntrospection } from './utils/sdlToIntrospection';
