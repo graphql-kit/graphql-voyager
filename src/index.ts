5a451d1ad564ff811ac7751e030f736c33fc133a
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-25 11:12:07 -0500

    fix import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as Voyager, type VoyagerProps } from './components/Voyager';
-export { voyagerIntrospectionQuery } from './utils/introspection-query
+export { voyagerIntrospectionQuery } from './utils/introspection-query';
 export { sdlToSchema } from './utils/sdl-to-introspection';
